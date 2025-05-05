5bb91b3c4132f337617447e1c90c2fb122610484
Sarumathy <sarumathy.ganapathy@gigamon.com>
2025-05-05 09:59:09 +0530

    Updated MongoDB Atlas and Docker File

--- server.js
+++ server.js
@@ -6,9 +6,8 @@ require('dotenv').config();
 const app = express();
 app.use(bodyParser.json());
 
-// Connect to MongoDB
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('Connected to MongoDB'))
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => console.log('Connected to MongoDB Atlas'))
     .catch((err) => console.error('MongoDB connection error:', err));
 
 // Example routes
